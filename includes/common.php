fc1feb65f1e624944dc66821c7272734618b2515
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2019-01-31 17:44:39 +0530

    index changed

--- includes/common.php
+++ includes/common.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "root", "mysql", "sae_event") or die(mysqli_error($con));
+$con = mysqli_connect("ec2-54-235-67-106.compute-1.amazonaws.com", "epbhlgadsoproh", "1ca18e9c25d1a874b0c99065bc566a031786bf3fb9cd81ed902914d4fe248aac", "ddr77ibfqdte2b") or die(mysqli_error($con));
 session_start();
 ?>
