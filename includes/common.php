26e49652a3847c002fad4ae59a3adbc9c14ab8fa
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2019-01-31 19:26:43 +0530

    common changed

--- includes/common.php
+++ includes/common.php
@@ -1,11 +1,11 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$server =us-cdbr-iron-east-03.cleardb.net;
+$username = b3b379651f5f1c;
+$password = f0464a4a;
+$db = heroku_f593c8177df17a1;
 
 $conn = new mysqli($server, $username, $password, $db);
-//session_start();
+session_start();
 ?>
