385902643bf07b14b416bd1158f31f69b2b266ee
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2019-01-31 20:58:08 +0530

    changes made

--- round1/additem.php
+++ round1/additem.php
@@ -20,5 +20,5 @@ $bal=$row2[0]-$row1[0];
 //$bal=$row1[0];
 $update_query="Update users set balance='$bal' where id='$user_id'";
 $update_query_res=mysqli_query($con,$update_query);
-echo $_SERVER['HTTP_REFERER']);
+header("location:javascript://history.go(-1)");
 ?>
