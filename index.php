dc029495bd35d1be90c0ced6796c5d9ed627293d
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2019-02-02 00:33:53 +0530

    changes made

--- index.php
+++ index.php
@@ -49,14 +49,14 @@ require 'includes/common.php';
                                               $sel="Select submit1,submit2,submit3,submit4,score,submit,qual,qual1,qual2,qual3 from users where id='$uid'";
                                               $selres=mysqli_query($con,$sel) or die(mysqli_error($con));
                                               $arr=mysqli_fetch_array($selres); ?>
-             <?php if($arr['submit']=='0' && ()){ ?>
+             <?php /*if($arr['submit']=='0' && ($i==6 || $i==11)){ ?>
 
 
                 <a class="btn btn-primary btn-xl js-scroll-trigger" href="autoquiz/autoquiz_set.php">Play now!</a>
-               <?php }
+               <?php } */
 
 
-            else  if($arr['submit']=='0' && $i<16 ){ ?>
+             if($arr['submit']=='0' && $i<16 ){ ?>
 
 
                 <a class="btn btn-primary btn-xl js-scroll-trigger" href="autoquiz/autoquiz.php?id=<?php echo $i; ?>">Play now!</a>
