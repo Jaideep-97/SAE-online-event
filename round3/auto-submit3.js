88ed12ed2c0c91e5d37f833737f4ef41161a760b
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2019-02-03 18:11:13 +0530

    changes made

--- round3/auto-submit3.js
+++ round3/auto-submit3.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 3, 2019 19:30:00").getTime();
+var countDownDate = new Date("Feb 3, 2019 19:45:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
